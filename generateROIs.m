8bd9344e9dda503c763dd9567b2bcff569268bc3
mpinkert <mpinkert@wisc.edu>
2017-05-09 17:16:26 -0500

    Info line
    Added a line to acquire the image info into a struct

--- generateROIs.m
+++ generateROIs.m
@@ -2,7 +2,9 @@ function  generateROIs()
 %UNTITLED2 Query the user to select an image file and then generate tiled
 %ROIs for that image.
 
-[fileName, pathName] = uigetfile({'*.tif'; '*.tiff';'*.*'});
+[fileName, pathName] = uigetfile({'*.tif';'*.*'});
+
+info = imfinfo(strcat(fileName,pathName));
 
 end
 
