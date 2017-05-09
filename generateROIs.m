3e6f702f5b289afa858a4ca740998c92b111051a
mpinkert <mpinkert@wisc.edu>
2017-05-09 17:10:54 -0500

    Get file implemented
    Implemented a line to get filename and filepath to the image

--- generateROIs.m
+++ generateROIs.m
@@ -2,7 +2,7 @@ function  generateROIs()
 %UNTITLED2 Query the user to select an image file and then generate tiled
 %ROIs for that image.
 
-filename = uigetfile();
+[fileName, pathName] = uigetfile({'*.tif'; '*.tiff';'*.*'});
 
 end
 
