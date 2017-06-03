7a085273d03b6003b08e55c4c60d9840b2ef34e4
mpinkert <mpinkert@wisc.edu>
2017-06-03 11:51:59 -0500

    Typos and naming
    Fixed some typos in the extractSubImages script and corrected the
    naming scheme

--- extractSubImages.m
+++ extractSubImages.m
@@ -31,16 +31,16 @@ else
 end
 
 %Define how big you want the sub files to be
-Param.subImageWidth = 512;
-Param.subImageHeight = 512;
+Param.subImgWidth = 512;
+Param.subImgHeight = 512;
 Param.ctBuffer = 4; %Number of border pixels for curvelet transform.
 %This generates an overlap region for the subimages, which allows analysis
 %over the full volume
 
 %Find out how many full size images there are
-Param.xImgNum = fix((Param.baseImgInfo.Width-2*Param.ctBuffer)/Param.subImageWidth);
-Param.yImgNum = fix((Param.baseImgInfo.Height-2*Param.ctBuffer)/Param.subImageWidth);
-Param.totalImgNum = xImgNum*yImgNum;
+Param.xImgNum = fix((Param.baseImgInfo.Width-2*Param.ctBuffer)/Param.subImgWidth);
+Param.yImgNum = fix((Param.baseImgInfo.Height-2*Param.ctBuffer)/Param.subImgWidth);
+Param.totalImgNum = Param.xImgNum*Param.yImgNum;
 
 %Define two thresholds to simplify analysis by eliminating empty images.
 Param.intensityThresh = 10; %Threshold for pixel intensity
@@ -48,8 +48,8 @@ Param.pixelNumThresh = 3000; %How many pixels must be valid to anlayze the image
 
 %Get the remainder to perform an offset, so as to obtain the center of the
 %image and delete the sides
-Param.xRem = rem(Param.baseImgInfo.Width,Param.subImageWidth);
-Param.yRem = rem(Param.baseImgInfo.Height,Param.subImageHeight);
+Param.xRem = rem(Param.baseImgInfo.Width - 2*Param.ctBuffer,Param.subImgWidth);
+Param.yRem = rem(Param.baseImgInfo.Height- 2*Param.ctBuffer,Param.subImgHeight);
 Param.xOffset = 1+fix(Param.xRem/2);
 Param.yOffset = 1+fix(Param.yRem/2);
 
@@ -58,12 +58,12 @@ Param.roiWidth = 64;
 Param.roiHeight = 64;
 
 %Calculate the number of ROIs and any necessary offset
-Param.xRoiNum = fix((Param.subImageWidth-2*Param.ctBuffer)/Param.roiWidth);
-Param.xRoiRem = rem(Param.subImageWidth-2*Param.ctBuffer,Param.roiWidth);
+Param.xRoiNum = fix(Param.subImgWidth/Param.roiWidth);
+Param.xRoiRem = rem(Param.subImgWidth,Param.roiWidth);
 Param.xRoiOffset = 1 + Param.ctBuffer + fix(Param.xRoiRem/2);
 
-Param.yRoiNum = fix((Param.subImageHeight-2*Param.ctBuffer)/Param.roiHeightt);
-Param.yRoiNum = rem((Param.subImageHeight-2*Param.ctBuffer,Param.RoiWidth);
+Param.yRoiNum = fix((Param.subImgHeight)/Param.roiHeight);
+Param.yRoiRem = rem((Param.subImgHeight),Param.roiHeight);
 Param.yRoiOffset = 1 + Param.ctBuffer + fix(Param.yRoiRem/2);
 
 %% Obtain sub-images and their ROIs
@@ -72,10 +72,10 @@ imgNum = 1;
 for y = 1:Param.yImgNum
     for x = 1:Param.xImgNum
         
-        xLow = Param.xOffset+imgWidth*(x-1);
-        yLow = Param.yOffset+imgHeight*(y-1);
-        xHigh = xLow + imgWidth-1+2*Param.ctBuffer;
-        yHigh = yLow + imgHeight-1+2*Param.ctBuffer;
+        xLow = Param.xOffset+Param.subImgWidth*(x-1);
+        yLow = Param.yOffset+Param.subImgHeight*(y-1);
+        xHigh = xLow + Param.subImgWidth-1+2*Param.ctBuffer;
+        yHigh = yLow + Param.subImgHeight-1+2*Param.ctBuffer;
        
         subImg = img(yLow:yHigh,xLow:xHigh);
         
@@ -83,7 +83,7 @@ for y = 1:Param.yImgNum
         if sum(sum(subImg>Param.intensityThresh)) > Param.pixelNumThresh %A little more than 1% of pixels for 512x512
             imgName = fullfile(subDir, sprintf('%s_%s.tif',Param.fileName_NE,num2str(imgNum)));
             imwrite(subImg,imgName);
-            Param = generateROIs(imgName, Param);
+            generateROIs(imgName, Param);
         end
         imgNum = imgNum+1;
     end
@@ -108,22 +108,23 @@ tempROI.date = datestr(now,'mm-dd-yyyy');
 tempROI.time = datestr(now,13);
 tempROI.shape = 1;
 
+roiNum = 1;
 
 for y = 1:Param.yRoiNum
     for x = 1:Param.xRoiNum
         roiName = strcat('ROI', int2str(roiNum));
         
         xLow = Param.xRoiOffset+Param.roiWidth*(x-1);
-        yLow = Param.yRoiOffset+Param.roiHeightt*(y-1);
+        yLow = Param.yRoiOffset+Param.roiHeight*(y-1);
         xHigh = xLow + Param.roiWidth-1;
-        yHigh = yLow + Param.roiHeightt-1;
+        yHigh = yLow + Param.roiHeight-1;
         
         separate_rois.(roiName) = tempROI;
-        separate_rois.(roiName).roi = [yLow,xLow,Param.roiHeightt,Param.roiWidth];
+        separate_rois.(roiName).roi = [yLow,xLow,Param.roiHeight,Param.roiWidth];
         separate_rois.(roiName).enclosing_rect = [yLow, xLow,...
             yHigh,xHigh];
         separate_rois.(roiName).xm = xLow+Param.roiWidth/2;
-        separate_rois.(roiName).ym = yLow+Param.roiHeightt/2;
+        separate_rois.(roiName).ym = yLow+Param.roiHeight/2;
         separate_rois.(roiName).boundary = ...
             {[xLow,yLow;...
             xLow,yHigh;...
