47f408a6526b9eb14c802d7b429658b59aff5dd8
mpinkert <mpinkert@wisc.edu>
2017-08-03 16:15:04 -0500

    Copies of old naming convention
    Next version of these scripts changes the naming conventions used.  I am
    uploading copies of the old version before updating the new.

---
+++ extractSubImages - Old name convention.m
@@ -0,0 +1,157 @@
+function extractSubImages()
+%% Function explanation
+% extractSubImages.m - extractSubImages is a module for the open-souorce
+% software tool CurveAlign.  This function facilitates automatic analysis
+% of large stitched images by splitting the image into a large number of
+% sub-images, and then automatically producing ROIs for each of those
+% sub-images.
+
+% By Laboratory for Optical and Computational Instrumentation, UW-Madison
+% Since 2017
+% Developers:
+%   Michael Pinkert (module developer, May 2017-)
+%   Yuming Liu (primary contact and lead developer, May 2017-)
+
+%% Open the image and define variables
+% Get the file and its information
+[fileName, filePath] = uigetfile({'*.tif';'*.*'},'Image to subdivide');
+originalDir = cd(filePath);
+img = imread(fileName);
+Param.baseImgInfo = imfinfo(fileName);
+[~, Param.fileName_NE] = fileparts(fileName); %File name without extension
+
+%Define the sub-image directory
+subDir = uigetdir('', 'Tiled images save directory');
+
+%Define how big you want the sub images to be
+
+
+userInput = inputdlg(...
+    {'Enter tiled image width:','Enter tiled image height',...
+        'Enter pixel overlap buffer:','Enter ROI width:', 'Enter ROI height:',...
+        'Enter grayscale intensity threshold:','Enter minimum number of pixels above threshold:'}, ...
+    'Tiled Image Parameters', ...
+    [1 50; 1 50; 1 50; 1 50; 1 50; 1 50; 1 50],...
+    {'512', '512', '4', '64', '64', '5', '500'});
+
+Param.subImgWidth = floor(str2double(userInput(1)));
+Param.subImgHeight  = floor(str2double(userInput(2)));
+Param.ctBuffer  = floor(str2double(userInput(3)));
+Param.roiWidth  = floor(str2double(userInput(4)));
+Param.roiHeight  = floor(str2double(userInput(5)));
+Param.intensityThresh  = floor(str2double(userInput(6)));
+Param.pixelNumThresh  = floor(str2double(userInput(7)));
+
+    
+%Param.ctBuffer explanation: 4 is the number of border pixels for curvelet transform.
+%This generates an overlap region for the subimages, which allows analysis
+%over the full volume.  May not be necessary to change.
+
+%Check for proper input values
+% todo: Find out how to do this, in a simple way.  
+
+%Find out how many sub images there are
+Param.xImgNum = fix((Param.baseImgInfo.Width-2*Param.ctBuffer)/Param.subImgWidth);
+Param.yImgNum = fix((Param.baseImgInfo.Height-2*Param.ctBuffer)/Param.subImgWidth);
+Param.totalImgNum = Param.xImgNum*Param.yImgNum;
+
+%Get the remainder to perform an offset, so as to obtain the center of the
+%image and delete the sides
+Param.xRem = rem(Param.baseImgInfo.Width - 2*Param.ctBuffer,Param.subImgWidth);
+Param.yRem = rem(Param.baseImgInfo.Height- 2*Param.ctBuffer,Param.subImgHeight);
+Param.xOffset = 1+fix(Param.xRem/2);
+Param.yOffset = 1+fix(Param.yRem/2);
+
+%Calculate the number of ROIs and any necessary offset
+Param.xRoiNum = fix(Param.subImgWidth/Param.roiWidth);
+Param.xRoiRem = rem(Param.subImgWidth,Param.roiWidth);
+Param.xRoiOffset = 1 + Param.ctBuffer + fix(Param.xRoiRem/2);
+
+Param.yRoiNum = fix((Param.subImgHeight)/Param.roiHeight);
+Param.yRoiRem = rem((Param.subImgHeight),Param.roiHeight);
+Param.yRoiOffset = 1 + Param.ctBuffer + fix(Param.yRoiRem/2);
+
+%% Obtain sub-images and their ROIs
+imgNum = 1;
+
+for y = 1:Param.yImgNum
+    for x = 1:Param.xImgNum
+        
+        xLow = Param.xOffset+Param.subImgWidth*(x-1);
+        yLow = Param.yOffset+Param.subImgHeight*(y-1);
+        xHigh = xLow + Param.subImgWidth-1+2*Param.ctBuffer;
+        yHigh = yLow + Param.subImgHeight-1+2*Param.ctBuffer;
+       
+        subImg = img(yLow:yHigh,xLow:xHigh);
+        
+        %Threshold for saving to stop analysis of empty regions
+        if sum(sum(subImg>Param.intensityThresh)) > Param.pixelNumThresh %A little more than 1% of pixels for 512x512
+            imgName = fullfile(subDir, sprintf('%s_%s.tif',Param.fileName_NE,num2str(imgNum)));
+            imwrite(subImg,imgName);
+            generateROIs(imgName, Param);
+        end
+        imgNum = imgNum+1;
+    end
+end
+
+%% Saving
+
+%Save the parameter file
+paramName = fullfile(subDir,strcat('Tiling Parameters-',Param.fileName_NE));
+save(paramName,'Param');
+
+%Return to the old directory
+cd(originalDir);
+
+end
+
+
+function generateROIs(imgName, Param)
+%Generate and save tiled ROIs for a specified image and parameters
+
+tempROI.date = datestr(now,'mm-dd-yyyy');
+tempROI.time = datestr(now,13);
+tempROI.shape = 1;
+
+roiNum = 1;
+
+for y = 1:Param.yRoiNum
+    for x = 1:Param.xRoiNum
+        roiName = strcat('ROI', int2str(roiNum));
+        
+        xLow = Param.xRoiOffset+Param.roiWidth*(x-1);
+        yLow = Param.yRoiOffset+Param.roiHeight*(y-1);
+        xHigh = xLow + Param.roiWidth-1;
+        yHigh = yLow + Param.roiHeight-1;
+        
+        separate_rois.(roiName) = tempROI;
+        separate_rois.(roiName).roi = [yLow,xLow,Param.roiHeight,Param.roiWidth];
+        separate_rois.(roiName).enclosing_rect = [yLow, xLow,...
+            yHigh,xHigh];
+        separate_rois.(roiName).xm = xLow+Param.roiWidth/2;
+        separate_rois.(roiName).ym = yLow+Param.roiHeight/2;
+        separate_rois.(roiName).boundary = ...
+            {[xLow,yLow;...
+            xLow,yHigh;...
+            xHigh,yHigh;...
+            xHigh,yLow;...
+            xLow,yLow]};
+            
+        roiNum = roiNum + 1;
+    end
+end
+
+[path,name,~] = fileparts(imgName);
+
+roiDir = fullfile(path,'ROI_management');
+if (~exist(roiDir,'dir'))
+    mkdir(roiDir)
+    fprintf('Folder created: %s \n',roiDir)
+end
+
+oldFolder = cd(roiDir);
+save(strcat(name,'_ROIs'),'separate_rois');
+
+cd(oldFolder);
+
+end
---
+++ reconstructSubImages - Old name convention.m
@@ -0,0 +1,129 @@
+function reconstructSubImages()
+%% Function explanation
+% reconstructSubImages.m - reconstructSubImages is a module for the
+% open-source software tool CurveAlign.  It is meant to be paired with the
+% function extractSubImages, after CurveAlign analysis has been run on the
+% resulting images.
+
+% This function takes the results from extractSubImages and
+% constructs new parametric images, using the ROIs defined in
+% extractSubImages as the pixel size.
+
+% By Laboratory for Optical and Computational Instrumentation, UW-Madison
+% Since 2017
+% Developers:
+%   Michael Pinkert (module developer, May 2017-)
+%   Yuming Liu (primary contact and lead developer, May 2017-)
+
+%% Image and batch selection
+close all
+
+%Query the user to select a file
+[fileName, filePath] = uigetfile({'*.tif';'*.*'},'Image to reconstruct');
+originalDir = cd(filePath);
+[~, fileName_NE] = fileparts(fileName); %File name without extension
+
+%Get and read the tiling parameters
+[paramFileName, paramFilePath] = uigetfile({'*.mat';'*.*'},'Tiling parameters');
+load(fullfile(paramFilePath,paramFileName));
+
+if ~(Param.xRoiNum)
+    disp('The parameter file is not formatted correctly')
+    return;
+elseif ~(Param.fileName_NE == fileName_NE)
+    disp('The tiling parameters do not match the chosen image.')
+    disp('Please check the file name parameter.')
+    return;
+end
+
+%Get and read the batch file
+[batchFile, batchFilePath] = uigetfile({'*.xlsx*','*.*'},'CurveAlign batch file');
+try [~,~,batchData] = xlsread(fullfile(batchFilePath,batchFile), 'CA ROI alignment analysis');
+catch
+    disp('Error: Batch data file is does not have the correct page.')
+    disp('The data should be on page: CA ROI alignment analysis');
+    return;
+end
+
+%Instantatiate the new parametric image.  Currently 3 dimensions, for
+%orientation (1), alignment (2), and feature number
+NewImg = nan(Param.yImgNum*Param.yRoiNum,Param.xImgNum*Param.xRoiNum,2);
+
+
+%% Image information extraction
+
+%Check to see if the analyzed images are for the correct base file
+if ~(batchData{2,2}(1,1:size(fileName_NE,2)) == fileName_NE)
+    disp('Error: This is not the correct batch file for %s', fileName)
+    disp('This batch file has images from %S', batchData{2,2})
+    return;
+end
+
+%Fill in pixels of NewImg where batchData has corresponding non-null values
+for row = 2:size(batchData,1)
+    if strcmp(batchData{row,4},'NaN')
+        continue;
+    end
+    
+    imgStr = batchData{row,2};
+    roiStr = batchData{row,3};
+    
+    imgNum = str2double(imgStr((size(fileName_NE,2)+2):size(imgStr,2)));
+    roiNum = str2double(roiStr(4:size(roiStr,2)));
+    
+    yImgIdx = floor((imgNum-1)/Param.xImgNum)*Param.yRoiNum ...
+        + ceil(roiNum/Param.xRoiNum);
+    xImgIdx = rem((imgNum-1),Param.xImgNum)*Param.xRoiNum ...
+        + rem((roiNum-1),Param.xRoiNum) + 1;
+    
+    NewImg(yImgIdx,xImgIdx,1) = batchData{row,4};
+    NewImg(yImgIdx,xImgIdx,2) = batchData{row,5};
+    NewImg(yImgIdx,xImgIdx,3) = batchData{row,6};
+    
+end
+
+%% Figure formation and saving
+
+analysisType = strcat(batchData{2,7},'_',num2str(Param.roiWidth),'x',num2str(Param.roiHeight));
+resultName = inputdlg('Base File Name:','Results Naming',[1 50],{strcat(fileName_NE,'_',analysisType)});
+
+
+resultsDir = fullfile(filePath,strcat(fileName_NE,' Results')); %make a results directory
+if ~(exist(resultsDir,'dir')) 
+    mkdir(resultsDir);
+end
+
+% todo : write code to check for prior results and stop overwriting if so
+save(fullfile(resultsDir,strcat(resultName{1},'.mat')),'NewImg','batchData'); %Save raw data
+cd(resultsDir)
+
+figNum = 1;
+while(ishandle(figNum))
+    figNum = figNum + 1;
+end
+
+figure(figNum)
+imagesc(NewImg(:,:,1))
+colormap('jet')
+title('Orientation')
+
+imgSize = size(NewImg);
+pbaspect([1 imgSize(1)/imgSize(2) 1]);
+
+axis off
+saveas(figure(figNum),fullfile(resultsDir,strcat(resultName{1},' Orientation.tif')))
+
+figure(figNum+1)
+imagesc(NewImg(:,:,2))
+colormap('jet')
+title('Alignment')
+
+imgSize = size(NewImg);
+pbaspect([1 imgSize(1)/imgSize(2) 1]);
+
+axis off
+saveas(figure(figNum+1),fullfile(resultsDir,strcat(resultName{1},' Alignment.tif')))
+
+cd(originalDir) %Return to the original directory
+
+end
