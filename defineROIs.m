ef3d4ff4061961b0548625f0b578121cea7b2acd
mpinkert <mpinkert@wisc.edu>
2018-02-12 13:25:05 -0600

    Ask ROI size once, name subfolder by ROI size
    The batch mode now only asks for ROIs once.
    
    In addition, the save folder is now
    ROI_Management_(ROIWidth)x(ROIHeight)

--- defineROIs.m
+++ defineROIs.m
@@ -1,6 +1,5 @@
-function Params = defineROIs(initParams)
+function Params = defineROIs()
     
-    Params = initParams;
 
     userInput = inputdlg(...
         {'Enter ROI width:', 'Enter ROI height:'}, ...
@@ -10,13 +9,5 @@ function Params = defineROIs(initParams)
 
     Params.roiWidth = floor(str2double(userInput(1)));
     Params.roiHeight = floor(str2double(userInput(2)));
-
-    Params.xRoiNum = fix(initParams.subImgWidth/Params.roiWidth);
-    Params.xRoiRem = rem(initParams.subImgWidth,Params.roiWidth);
-    Params.xRoiOffset = 1 + initParams.BorderSize + fix(Params.xRoiRem/2);
-
-    Params.yRoiNum = fix((initParams.subImgHeight)/Params.roiHeight);
-    Params.yRoiRem = rem((initParams.subImgHeight),Params.roiHeight);
-    Params.yRoiOffset = 1 + initParams.BorderSize + fix(Params.yRoiRem/2);
 
 return
