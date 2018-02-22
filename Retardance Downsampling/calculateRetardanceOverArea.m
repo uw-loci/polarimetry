6a66b0d38c3c54dba5e257a9150dfaec9cbe5d74
mpinkert <mpinkert@wisc.edu>
2018-02-22 16:29:28 -0600

    Moved calculate retardance function to new folder
    Moved it to a new folder so that all functions for a single script can
    be in one place.

--- Retardance Downsampling/calculateRetardanceOverArea.m
+++ Retardance Downsampling/calculateRetardanceOverArea.m
@@ -1,10 +1,10 @@
 function [retMag,retAngle] = calculateRetardanceOverArea(retardance, orientation)
-    
+    %
     % This gives me the orientation in 360 degrees, doubled to calculate alignment.
     circularOrientation = (2*pi/180)*(double(orientation)/100);
     complexOrientation = exp(1i*circularOrientation);
     
-    retardanceWeightedByOrientation = retardance.*complexOrientation;
+    retardanceWeightedByOrientation = double(retardance).*complexOrientation;
     
     numPixels = numel(retardance);
     
